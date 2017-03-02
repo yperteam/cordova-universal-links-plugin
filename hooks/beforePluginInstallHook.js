44a76500e70169ce2ccc29945888fcdf77d12741
Antoine KNOCKAERT <antoine@yper.fr>
2017-03-02 17:58:47 +0100

    FIX: Trying to fix plugin build with meteorjs

--- hooks/beforePluginInstallHook.js
+++ hooks/beforePluginInstallHook.js
@@ -46,6 +46,10 @@ module.exports = function(ctx) {
   console.log(JSON.stringify(pluginNpmDependencies, null, 2));
 
   var npm = (process.platform === "win32" ? "npm.cmd" : "npm");
+
+  var result = spawnSync(npm, ['--version'], { });
+  console.log(result);
+
   var result = spawnSync(npm, ['install', '--production'], { cwd: './plugins/' + ctx.opts.plugin.id });
   if (result.error) {
     throw result.error;
